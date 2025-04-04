e9e12bff16ef4af9fbdb52570528007e344e588a
Renz <67616736+Orinchin@users.noreply.github.com>
2025-04-04 23:42:26 +0800

    Update hud.lua

--- client/hud.lua
+++ client/hud.lua
@@ -56,3 +56,50 @@ CreateThread(function()
 		Wait(200)
 	end
 end)
+
+function GetAccountIndexByName(accounts, accountName)
+    for i, account in ipairs(accounts) do
+        if account.name == accountName then
+            return i
+        end
+    end
+    return nil
+end
+
+CreateThread(function()
+    while true do
+        if ESX.PlayerLoaded then
+            local playerData = ESX.GetPlayerData()
+
+            local hours = GetClockHours()
+            local minutes = GetClockMinutes()
+
+            local ampm = "AM"
+            if hours >= 12 then
+                ampm = "PM"
+                if hours > 12 then
+                    hours = hours - 12
+                end
+            end
+
+            local formattedMinutes = string.format("%02d", minutes)
+            local formattedHours = string.format("%02d", hours)
+
+            SendMessage('setPlayerJob', playerData.job.label .. " - " .. playerData.job.grade_label)
+            SendMessage('setPlayerTime', formattedHours .. ":" .. formattedMinutes .. ' | ' .. ampm)
+
+            local bankAccountIndex = GetAccountIndexByName(playerData.accounts, 'bank')
+            local bankMoney = bankAccountIndex and playerData.accounts[bankAccountIndex].money or 0
+
+            local cashAccountIndex = GetAccountIndexByName(playerData.accounts, 'money')
+            local cashMoney = cashAccountIndex and playerData.accounts[cashAccountIndex].money or 0
+
+            SendMessage('setPlayerBank', bankMoney)
+            SendMessage('setPlayerCash', cashMoney)
+
+            Wait(500)
+        else
+            Wait(1000)
+        end
+    end
+end)
